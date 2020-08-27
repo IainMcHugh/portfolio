fa4013402b01a03cb2523b9508f358d4ab23ff4c
IainMcHugh <46798029+IainMcHugh@users.noreply.github.com>
2020-08-27 16:38:11 +0100

    Updates

---
+++ precache-manifest.975396ef2e52fb365382103561fbe3db.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb0f0b831151808a2c550b56836f3527",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ba0470b04d8b847c7671",
+    "url": "/portfolio/static/css/main.37e63b51.chunk.css"
+  },
+  {
+    "revision": "7ed03de8144781ac44ae",
+    "url": "/portfolio/static/js/2.8b19ddfc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.8b19ddfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba0470b04d8b847c7671",
+    "url": "/portfolio/static/js/main.bd47e006.chunk.js"
+  },
+  {
+    "revision": "4c76482b968eb9f47af6",
+    "url": "/portfolio/static/js/runtime-main.98fb296d.js"
+  },
+  {
+    "revision": "39e0bd6cb0065e115f28178fbaf6b179",
+    "url": "/portfolio/static/media/iain-animate-stationary.39e0bd6c.png"
+  },
+  {
+    "revision": "850431e4b05a180ffc41e216e2b2c6f4",
+    "url": "/portfolio/static/media/rock-bg.850431e4.png"
+  },
+  {
+    "revision": "5c96c12e5c5f42607c3f0bca64740c88",
+    "url": "/portfolio/static/media/sunset-bg.5c96c12e.png"
+  }
+]);
